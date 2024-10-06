6a8659343055e7d4f07f63094e7785d491a86dec
Gustavo Carvalho <gustavo1net@hotmail.com>
2024-10-06 15:51:41 -0300

    Layot base

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,35 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import MainMenu from './components/menus/MainMenu.vue';
+import Calculator from './views/Calculator.vue';
 </script>
 
 <template>
-  <div>
+  <div class="main-app">
     <MainMenu />
-  </div>
-  <div>
-    <a href="https://www.electronjs.org/" target="_blank">
-      <img src="./assets/electron.svg" class="logo electron" alt="Electron logo" />
-    </a>
-    <a href="https://vitejs.dev/" target="_blank">
-      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Electron + Vite + Vue" />
-  <div class="flex-center">
-    Place static files into the <code>/public</code> folder
-    <img style="width: 2.4em; margin-left: .4em;" src="/logo.svg" alt="Logo">
+    <Calculator />
   </div>
 </template>
 
-<style>
-.flex-center {
+<style lang="scss">
+@import './assets/scss/reset.scss';
+
+body{
+  background-color: #1e1f22;
+  max-height: 100vh;
+}
+
+.main-app{
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: stretch;
+  max-height: 100%;
+  min-height: 1px;
+  height: 100%;
 }
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+#app{
+  height: 100vh;
+  overflow-y: hidden;
 }
 
-.logo.electron:hover {
-  filter: drop-shadow(0 0 2em #9FEAF9);
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/views/Calculator.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="calculator">
+		<ul>
+			<li class="calculator-line" v-for="(line, i) in 10" :key="'calc_line_' + i">
+				<p class="line">{{ i }}.</p>
+				<input type="text" class="input" v-model="textRef">
+				<p class="result">{{ i * 5 }}</p>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	data(){
+		return {
+			textRef: '$a = 5'
+		}
+	}
+});
+</script>
+
+<style lang="scss" scoped>
+.calculator {
+	background: #2b2d31;
+	padding-top: 10px;
+	overflow-y: auto;
+	min-height: 1px;
+	height: 100%;
+	max-height: 100%;
+	flex: 1;
+}
+.calculator-line{
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 10px;
+	gap: 10px;
+}
+.line{
+	color: #C3C3C3;
+	font-size: 14px;
+	text-align: left;
+}
+.input{
+	background: none;
+	border: none;
+	color: #3FB27F;
+	font-size: 14px;
+	width: 100%;
+
+	&:focus{
+		outline: none;
+	}
+}
+
+.result{
+	color: #C3C3C3;
+	font-size: 14px;
+	text-align: right;
+}
+</style>
