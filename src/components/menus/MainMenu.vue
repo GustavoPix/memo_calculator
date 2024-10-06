32add46fe0424c6de467682236f0f0886c986ae0
Gustavo Carvalho <gustavo1net@hotmail.com>
2024-10-06 14:38:35 -0300

    first commit with boilerplate

---
+++ src/components/menus/MainMenu.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="main-menu">
+		<h1>Main Menu</h1>
+		<ul>
+			<li><a href="#home">Home</a></li>
+			<li><a href="#about">About</a></li>
+			<li><a href="#contact">Contact</a></li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'MainMenu'
+}
+</script>
+
+<style scoped>
+.main-menu {
+	text-align: center;
+}
+
+.main-menu ul {
+	list-style-type: none;
+	padding: 0;
+}
+
+.main-menu li {
+	display: inline;
+	margin: 0 10px;
+}
+
+.main-menu a {
+	text-decoration: none;
+	color: #333;
+}
+
+.main-menu a:hover {
+	text-decoration: underline;
+}
+</style>
